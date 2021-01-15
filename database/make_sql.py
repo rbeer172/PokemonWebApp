073a10108748c8f2f12b99e3c2c653d2d868ad85
rbeer172 <70540400+rbeer172@users.noreply.github.com>
2021-01-14 23:39:05 -0600

    Added function to generate sql for pokemon evolution
    python script can now generate a sql transaction to add a pokemon evolution chain to the database

--- database/make_sql.py
+++ database/make_sql.py
@@ -5,11 +5,12 @@ import inspect
 with open('password.txt') as f:
     lines = [line.rstrip() for line in f]
     
-username = lines[0]
-pg_password = lines[1]
-databaseName = lines[2]
-hostAddress = lines[3]
-conn = psycopg2.connect(database = databaseName, user = username, password = pg_password, host = hostAddress)
+conn = psycopg2.connect(
+    database = lines[2], 
+    user = lines[0], 
+    password = lines[1], 
+    host = lines[3])
+
 cursor = conn.cursor()
 
 def newMove():
@@ -71,7 +72,8 @@ def newPokemon():
         BEGIN
         INSERT INTO pokemon_db.pokemon VALUES(DEFAULT,%s, '%s', '%s', %s, %s, '%s', '%s', '%s');
         INSERT INTO pokemon_db.pokemon_stats VALUES(%s, %s, %s, %s, %s, %s, %s);
-        """ % (pokedex, name, species, height, weight, growthRate, eggGroup, description, hp, attack, defense, specialAttack, specialDefense, speed, total)
+        """ % (pokedex, name, species, height, weight, growthRate, eggGroup, description,
+            hp, attack, defense, specialAttack, specialDefense, speed, total)
 
         cursor.execute("SELECT COUNT(*) FROM pokemon_db.pokemon;")
         currentID = cursor.fetchone()
@@ -123,7 +125,77 @@ def newPokemon():
             break
 
 def newEvolution():
-    pass
+    chainNum = int(input("enter number of pokemon in chain: "))
+    sql = "BEGIN\nINSERT INTO pokemon_db.evolution_group VALUES(DEFAULT);\n"
+
+    for i in range(0,chainNum):
+        pokemonName = input("enter pokemon name:")
+        order = input("enter the pokemon's position in the chain(number): ")
+        cursor.execute("select pokemon_id from pokemon_db.pokemon where pokemon_name = " + pokemonName)
+        pokemonID = cursor.fetchone()
+        cursor.execute("SELECT nextval(pg_get_serial_sequence('pokemon_db.evolution_group', 'group_id'));")
+        nextID = cursor.fetchone()
+
+        print("\t\n1.level-up\n2.item\n3.friendship\n4.trade\n5.other\n")
+        evolutionType = int(input("select a evolution option(number): "))
+        if evolutionType == 1:
+            level = input("enter level: ")
+            sql = sql + """INSERT INTO pokemon_db.pokemon_evolution 
+            VALUES(%s, %s, %s, %s, NULL, NULL, NULL, FALSE, NULL ,FALSE, NULL)
+            """ % (pokemonID[0], nextID[0], order, level)
+
+        elif evolutionType == 2:
+            item = input("enter item name: ")
+            sql = sql + """INSERT INTO pokemon_db.pokemon_evolution 
+            VALUES(%s, %s, %s, NULL, %s, NULL, NULL, FALSE, NULL ,FALSE, NULL)
+            """ % (pokemonID[0], nextID[0], order, item)
+            
+        elif evolutionType == 3:
+            option = input("Does the evolution happen at certain time of day? (y/n): ")
+            if option == "y":
+                time = input("enter daytime or nighttime: ")
+                sql = sql + """INSERT INTO pokemon_db.pokemon_evolution 
+            VALUES(%s, %s, %s, NULL, NULL, NULL, '%s', TRUE, NULL ,FALSE, NULL)
+            """ % (pokemonID[0], nextID[0], order, time)
+            else:
+                sql = sql + """INSERT INTO pokemon_db.pokemon_evolution 
+            VALUES(%s, %s, %s, NULL, NULL, NULL, NULL, TRUE, NULL ,FALSE, NULL)
+            """ % (pokemonID[0], nextID[0], order)
+
+        elif evolutionType == 4:
+            option = input("Does the pokemon need a held item? (y/n): ")
+            if option == "y":
+                item = input("enter item name: ")
+                sql = sql + """INSERT INTO pokemon_db.pokemon_evolution 
+            VALUES(%s, %s, %s, NULL, '%s', NULL, NULL, FALSE, NULL ,TRUE, NULL)
+            """ % (pokemonID[0], nextID[0], order, item)
+            else:
+                sql = sql + """INSERT INTO pokemon_db.pokemon_evolution 
+            VALUES(%s, %s, %s, NULL, NULL, NULL, NULL, FALSE, NULL ,TRUE, NULL)
+            """ % (pokemonID[0], nextID[0], order)
+
+        elif evolutionType == 5:
+            cursor.execute("Select * FROM pokemon_db.pokemon_evolution limit 0;")
+            columnNames = [desc[0] for desc in cursor.description]
+            for j in range(0,len(columnNames)):
+                insertQuery = "INSERT INTO pokemon_db.pokemon_evolution VALUES("
+                columnQuery = "select data_type from information_schema.columns where column_name = '%s';" % (columnNames[j])
+                cursor.execute(columnQuery)
+                result = cursor.fetchone()
+
+                value = input("enter " + columnNames[j] + "(enter NULL if not applicable): ")
+                if result[0] == "integer" or value == "NULL":
+                    insertQuery = insertQuery + value
+                else:
+                    insertQuery = insertQuery + "'" + value + "'"
+                if j != len(columnNames) - 1:
+                    insertQuery = insertQuery + ","
+            sql = sql + insertQuery + ");"
+
+    file = open("evolution.sql", "a")
+    file.write(sql)
+    file.close()
+
 
 def addToTable():
     cursor.execute("""SELECT table_name FROM information_schema.tables WHERE table_schema = 'pokemon_db'
@@ -181,7 +253,7 @@ def addToTable():
 optionList = """\nselect an option\n
             1.new move
             2.new pokemon
-            3.new evolution chain
+            3.new evolution chain(all pokemon must be added first)
             4.add to table"""
 print("Pokmon database sql maker\n" + optionList)
 option = int(input("\nNumber: "))
