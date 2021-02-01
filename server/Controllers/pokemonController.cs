0dcc4994c2752a1b300e2a749e259dc9081743ae
rbeer172 <70540400+rbeer172@users.noreply.github.com>
2021-02-01 16:29:48 -0600

    Add web API server

---
+++ server/Controllers/pokemonController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class pokemonController : ControllerBase
+    {}
+}
